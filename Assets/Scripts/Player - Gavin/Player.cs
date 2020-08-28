217ffad69b8f6f1157388ead7a8b67dbc2979131
William Gavin Lacalle <digitalbros7@gmail.com>
2020-08-28 01:39:06 -0500

    Adds player actions for input system.

--- Assets/Scripts/Player - Gavin/Player.cs
+++ Assets/Scripts/Player - Gavin/Player.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayerMovement playerMovement;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        playerMovement = GetComponent<PlayerMovement>();
     }
 }
