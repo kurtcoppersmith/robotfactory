55bb123d6dd6f1eb324adb88898744578d92f94b
Andrew Ward <andrewward22499@gmail.com>
2020-08-31 15:56:52 -0500

    Adds crate manager, crates, and crate stands.

---
+++ Assets/Scripts/Crate/Crate.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public enum Colors{Red, Blue, Green}
+    private float timer;
+    private Material materialColor;
+    private CrateManager manager;
+    public bool delivered;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //manager
+        if (GameObject.Find("Crate Manager") != null)
+            manager = GameObject.Find("Crate Manager").GetComponent<CrateManager>();
+        else
+            Debug.LogError("Unable to find Crate Manager");
+        //material
+        materialColor = this.gameObject.GetComponent<Renderer>().material;
+        //set crate color
+        SpawnColor();
+        //set crate timer
+        timer = manager.duration;
+        //
+        delivered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lower crate timer
+        timer -= Time.deltaTime;
+        //call explode when timer = 0;
+        if(timer<=0 && !delivered)
+        {
+            manager.Explode();
+            Debug.Log("Time is up");
+        }
+    }
+
+    private void SpawnColor()
+    {
+        switch((Colors)Random.Range(0,3))
+        {
+            case Colors.Blue:
+                color = Color.blue;
+                materialColor.color = Color.blue;
+                break;
+            case Colors.Green:
+                color = Color.green;
+                materialColor.color = Color.green;
+                break;
+            case Colors.Red:
+                color = Color.red;
+                materialColor.color = Color.red;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Crate/CrateManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateManager : MonoBehaviour
+{
+    //spawn time in seconds
+    public float SpawnTime;
+    //time till next spawn
+    private float RemaningSpawnTime;
+    //time till explosion
+    public float duration;
+    //crate prefab for instantiating
+    public GameObject Crate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCrate();
+        RemaningSpawnTime = SpawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(RemaningSpawnTime<=0)
+        {
+            SpawnCrate();
+            RemaningSpawnTime = SpawnTime;
+        }
+        RemaningSpawnTime -= Time.deltaTime;
+    }
+
+    void SpawnCrate()
+    {
+        Instantiate(Crate, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+
+    public void Explode()
+    {
+        //Call Game over 
+        Debug.Log("You Lose Game Over");
+    }
+
+    public void DeliverCrate()
+    {
+        Debug.Log("Crate Delivered");
+    }
+}
