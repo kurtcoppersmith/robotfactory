b25bb018c6d2e2c248698f0cca2aa9c4346df293
Luis Hernandez <luis777_hdz_11@hotmail.com>
2020-10-06 00:38:18 -0500

    WIP powrup

--- Assets/Scripts/Misc/Powerups.cs
+++ Assets/Scripts/Misc/Powerups.cs
@@ -6,20 +6,23 @@ public class Powerups : MonoBehaviour
 {
     public enum Enhancements
     {
-        Robotic_Strength,
-        Robotic_Speed,
-        Robotic_Shield,
+        Robotic_Pulse,
+        Robotic_Boost,
+        Robotic_Chasis,
+        Robotic_Enchancements,
         None
     }
 
     public Enhancements augments;
     private PlayerMovement playerVar;
+    private GameManager gm;
 
     // Start is called before the first frame update
     void Start()
     {
         augments = Enhancements.None;
         playerVar = FindObjectOfType<PlayerMovement>();
+        gm = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
@@ -27,16 +30,19 @@ public class Powerups : MonoBehaviour
     {
         switch(augments)
         {
-            case Enhancements.Robotic_Strength:
-                //double pick up!
-                Debug.Log("stronk!");
+            case Enhancements.Robotic_Pulse:
+                //add time!
+                gm.addTime(15f);
+                Debug.Log("time_looop!");
                 break;
-            case Enhancements.Robotic_Speed:
+            case Enhancements.Robotic_Boost:
                 //Speed up!
+                playerVar.SetPlayerSped(true);
                 Debug.Log("IAMSPEED!");
                 break;
-            case Enhancements.Robotic_Shield:
+            case Enhancements.Robotic_Chasis:
                 //NOU!
+                playerVar.SetPlayerChasis(true);
                 Debug.Log("PROTECC!");
                 break;
             default:
@@ -47,5 +53,10 @@ public class Powerups : MonoBehaviour
     public Enhancements getEnhancement()
     {
         return augments;
+    }
+
+    public void setEnhancement(Enhancements effect)
+    {
+        augments = effect;
     }
 }
