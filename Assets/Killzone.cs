88f7dd617889194db160743cd06c5176f0dc4e7b
Luis Hernandez <luis777_hdz_11@hotmail.com>
2020-10-07 15:34:27 -0500

    lavalevel

---
+++ Assets/Killzone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killzone : MonoBehaviour
+{
+    public GameObject origin;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = origin.transform.position;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.position = origin.transform.position;
+    }
+}
