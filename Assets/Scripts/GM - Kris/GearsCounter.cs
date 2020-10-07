258332942d83c471816efa7a4f45dd1f737d038b
Kris J <KrisJohnson458@gmail.com>
2020-10-07 01:44:52 -0500

    Gears UI and Save

---
+++ Assets/Scripts/GM - Kris/GearsCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GearsCounter : MonoBehaviour
+{
+    public TextMeshProUGUI gearsText;
+
+    void Start()
+    {
+        gearsText.text = $"Gears: { GameManager.Instance.returnGears()}";
+    }
+
+    void Update()
+    {
+        if (!GameManager.Instance.isPaused && !GameManager.Instance.hasEnded)
+        {
+            gearsText.text = $"Gears: { GameManager.Instance.returnGears()}";
+        }
+        else
+        {
+            gearsText.text = "";
+        }
+    }
+}
--- Assets/Scripts/Misc/SaveData.cs
+++ Assets/Scripts/Misc/SaveData.cs
@@ -9,12 +9,14 @@ public class SaveData
     public int score;
     public float time;
     public float[] position;
+    public int gears;
 
 
     public SaveData(GameManager gm)
     {
         score = gm.returnScore();
         time = gm.returnTime();
+        gears = gm.returnGears();
       //  position = gm.returnPlayerPos();
     }
 }
