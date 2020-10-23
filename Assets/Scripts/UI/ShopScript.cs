053cbbcdb24b98abc7beee1cbd4a48814cbfb161
Andrew Ward <andrewward22499@gmail.com>
2020-10-23 14:20:38 -0500

    Small push

--- Assets/Scripts/UI/ShopScript.cs
+++ Assets/Scripts/UI/ShopScript.cs
@@ -6,12 +6,10 @@ using UnityEngine.UI;
 [System.Serializable]
 public struct ItemInfo
 {
-    public string name;
     public Text nameText;
-    public int cost;
     public Text costText;
-    public Sprite itemSprite;
     public Image itemImage;
+    public int itemNumber;
 }
 
 
@@ -21,21 +19,23 @@ public class ShopScript : MonoBehaviour
     public ItemInfo item1;
     public ItemInfo item2;
     public ItemInfo item3;
+    private PowerUpManager item;
     // Start is called before the first frame update
     void Start()
     {
+        item = PowerUpManager.Instance;
         //Set item names
-        item1.nameText.text = item1.name;
-        item2.nameText.text = item2.name;
-        item3.nameText.text = item3.name;
+        item1.nameText.text = item.powerUps[item1.itemNumber].name;
+        item2.nameText.text = item.powerUps[item2.itemNumber].name;
+        item3.nameText.text = item.powerUps[item3.itemNumber].name;
         //set item cost text
-        item1.costText.text = "Cost: " + item1.cost + " gear(s)";
-        item2.costText.text = "Cost: " + item2.cost + " gear(s)";
-        item3.costText.text = "Cost: " + item3.cost + " gear(s)";
+        item1.costText.text = "Cost: " + item.powerUps[item1.itemNumber].cost + " gear(s)";
+        item2.costText.text = "Cost: " + item.powerUps[item2.itemNumber].cost + " gear(s)";
+        item3.costText.text = "Cost: " + item.powerUps[item3.itemNumber].cost + " gear(s)";
         //set item image
-        item1.itemImage.sprite = item1.itemSprite;
-        item2.itemImage.sprite = item2.itemSprite;
-        item3.itemImage.sprite = item3.itemSprite;
+        item1.itemImage.sprite = item.powerUps[item1.itemNumber].itemSprite;
+        item2.itemImage.sprite = item.powerUps[item2.itemNumber].itemSprite;
+        item3.itemImage.sprite = item.powerUps[item3.itemNumber].itemSprite;
     }
 
     public void Buy(int itemNumber)
@@ -44,13 +44,13 @@ public class ShopScript : MonoBehaviour
         switch(itemNumber)
         {
             case 1:
-                price = item1.cost;
+                price = item.powerUps[item1.itemNumber].cost;
                 break;
             case 2:
-                price = item1.cost;
+                price = item.powerUps[item2.itemNumber].cost;
                 break;
             case 3:
-                price = item1.cost;
+                price = item.powerUps[item3.itemNumber].cost;
                 break;
             default:
                 Debug.Log("Invalid itemNumber");
@@ -59,20 +59,12 @@ public class ShopScript : MonoBehaviour
         if(GameManager.Instance.returnGears() - price >= 0)
         {
             GameManager.Instance.setGears(GameManager.Instance.returnGears() - price);
-            //DefaultInfo(info);
+            item.powerUps[itemNumber - 1].unlocked = true;
         }
         else
         {
             Debug.Log("Not enough Gears");
         }
     }
-
-    void DefaultInfo(ItemInfo info)
-    {
-        info.name = "";
-        info.cost = 0;
-        info.itemImage = null;
-    }
-
     
 }
