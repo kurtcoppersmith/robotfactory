8b3ab1cd1c848df5946c1ecb02bb0cb76646c10b
William Gavin Lacalle <digitalbros7@gmail.com>
2020-10-26 23:27:29 -0500

    Begins adding the audio manager

---
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : SingletonMonoBehaviour<MusicManager>
+{
+    public AudioSource basicAudioSource;
+    public AudioClip panicMusic;
+
+    public float fadeInTime = 3;
+    public float timeUntilPanicMusic = 60f;
+
+    new void Awake()
+    {
+        if (Instance != this)
+        {
+            Instance.basicAudioSource.clip = this.basicAudioSource.clip;
+        }
+
+        base.Awake();
+
+        basicAudioSource.Play();
+    }
+
+    void Start()
+    {
+        if (Instance == this)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/SoundEffectsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectsManager : SingletonMonoBehaviour<SoundEffectsManager>
+{
+    public AudioSource soundEffectSource;
+    public List<AudioClip> audioClips = new List<AudioClip>();
+    private Dictionary<string, AudioClip> namedAudioClips = new Dictionary<string, AudioClip>();
+
+    new void Awake()
+    {
+        base.Awake();
+
+        for (int i = 0; i < audioClips.Count; i++)
+        {
+            namedAudioClips.Add(audioClips[i].name, audioClips[i]);
+        }
+    }
+
+    public void Play(string audioClipName)
+    {
+        if(audioClipName == null || audioClipName == "")
+        {
+            return;
+        }
+
+        soundEffectSource.spatialize = false;
+        soundEffectSource.PlayOneShot(namedAudioClips[audioClipName]);
+    }
+}
