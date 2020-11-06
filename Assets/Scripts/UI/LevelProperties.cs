99af0234f1d170db3c0b58e2176d4713cb3dcdf3
William Gavin Lacalle <digitalbros7@gmail.com>
2020-11-05 23:27:09 -0600

    LevelPropertyBeginnings

---
+++ Assets/Scripts/UI/LevelProperties.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelProperties : MonoBehaviour
+{
+    public Image gear1;
+    public Image gear2;
+    public Image gear3;
+    public TMPro.TextMeshProUGUI highScoreMesh;
+
+    void Start()
+    {
+        //gear1 = Game
+    }
+}
