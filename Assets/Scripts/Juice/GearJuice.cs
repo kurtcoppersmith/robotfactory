7a3822fa381bad5f6134af17faf2fc1e643847da
William Gavin Lacalle <digitalbros7@gmail.com>
2020-11-09 05:46:24 -0600

    Adds some gear juice

---
+++ Assets/Scripts/Juice/GearJuice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearJuice : MonoBehaviour
+{
+    public bool turnRight = true;
+    public float speed = 10;
+
+    void Update()
+    {
+        if (turnRight)
+        {
+            transform.Rotate(0, 0, -speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(0, 0, speed * Time.deltaTime);
+        }
+        
+    }
+}
