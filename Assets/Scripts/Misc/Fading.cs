8525c1db86d1ebf163c66128eedf32abd3ad6670
Luis Hernandez <luis777_hdz_11@hotmail.com>
2020-11-11 14:07:21 -0600

    a

--- Assets/Scripts/Misc/Fading.cs
+++ Assets/Scripts/Misc/Fading.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Fading : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator anim;
+    private Image panel;
+
     void Start()
     {
+        anim = GetComponent<Animator>();
+        panel = GetComponent<Image>();
+        panel.enabled = false;
+
+    }
+
+    public void fadeIn()
+    {
+        panel.enabled = true;
+        StartCoroutine(fadingTimer(2f));
+    }
+
+    public void fadeOut()
+    {
+        anim.SetBool("isFading", false);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator fadingTimer(float t)
     {
-        
+        yield return new WaitForSeconds(t);
+
+
+        anim.SetBool("isFading", true);
     }
 }
