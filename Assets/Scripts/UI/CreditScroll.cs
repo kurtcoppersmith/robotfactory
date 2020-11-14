2cd21bc3d431b53a9c591382569a22f07efa9496
William Gavin Lacalle <digitalbros7@gmail.com>
2020-11-14 03:51:09 -0600

    Adds scrolling credits

---
+++ Assets/Scripts/UI/CreditScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CreditScroll : MonoBehaviour
+{
+    public Transform startingPosition;
+    public Transform endingPosition;
+    public GameObject textToScroll;
+    public float scrollSpeed;
+
+    void OnEnable()
+    {
+        textToScroll.transform.position = startingPosition.position;
+    }
+
+    void Update()
+    {
+        textToScroll.transform.position = Vector3.Lerp(textToScroll.transform.position, endingPosition.position, scrollSpeed * Time.deltaTime);
+    }
+
+}
