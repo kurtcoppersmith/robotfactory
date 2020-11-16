55483e9b23b30d6a42bf99eabf4dcc49d0c69d52
Luis Hernandez <luis777_hdz_11@hotmail.com>
2020-11-16 04:08:15 -0600

    Adds intro Splash

---
+++ Assets/Scripts/Misc/videoScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class videoScript : MonoBehaviour
+{
+    int nextScene = 0;
+    public Animator anim;
+
+    void Start()
+    {
+        StartCoroutine(sceneLoader());
+        //anim.GetComponent<Animator>();
+    }
+
+    IEnumerator sceneLoader()
+    {
+        yield return new WaitForSeconds(3f);
+        nextScene++;
+        SceneManager.LoadScene(nextScene);
+    }
+}
