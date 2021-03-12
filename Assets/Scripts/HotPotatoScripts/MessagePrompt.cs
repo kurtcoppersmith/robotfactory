a0eef16d31c46bc4870e82e0961f033e065f9b74
Luis Hernandez <luis777_hdz_11@hotmail.com>
2021-03-11 19:50:28 -0600

    Environment WIP
    wip

---
+++ Assets/Scripts/HotPotatoScripts/MessagePrompt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagePrompt : MonoBehaviour
+{
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        canvas.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        canvas.SetActive(false);
+    }
+}
