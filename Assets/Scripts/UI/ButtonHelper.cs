83a1cbecc865563989214c046c30220b32b487a6
William Gavin Lacalle <digitalbros7@gmail.com>
2021-04-15 12:58:08 -0500

    Checks music and adds sound effects to all menu buttons

---
+++ Assets/Scripts/UI/ButtonHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHelper : MonoBehaviour
+{
+    public void PlayMenuSound()
+    {
+        SoundEffectsManager.Instance.Play("Menu_Select");
+    }
+}
