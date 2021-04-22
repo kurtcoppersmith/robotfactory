86e121fa854c603fc71068216b620b744a7b8be1
William Gavin Lacalle <digitalbros7@gmail.com>
2021-04-22 02:44:00 -0500

    Adds movement juice

---
+++ Assets/Scripts/HotPotatoScripts/JuiceCrate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class JuiceCrate : MonoBehaviour
+{
+    public List<Transform> movementPath = new List<Transform>();
+    public float startTime = 0f;
+    public float crateSpeed = 0f;
+    bool shouldCountDown = true;
+
+    private void Update()
+    {
+        if (shouldCountDown)
+        {
+            startTime -= Time.deltaTime;
+            if (startTime <= 0)
+            {
+                Vector3 startingPos = transform.position;
+
+                Sequence moveSequence = DOTween.Sequence();
+
+                for (int i = 0; i < movementPath.Count; i++)
+                {
+                    moveSequence.Append(transform.DOMove(movementPath[i].transform.position, crateSpeed));
+                }
+
+                if (movementPath.Count > 0)
+                {
+                    moveSequence.Append(transform.DOMove(startingPos, crateSpeed));
+                }
+
+                moveSequence.SetLoops(-1);
+                moveSequence.Play();
+
+                shouldCountDown = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HotPotatoScripts/JuiceFlo.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class JuiceFlo : MonoBehaviour
+{
+    public List<Transform> movementPath = new List<Transform>();
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+
+        if (movementPath.Count > 0)
+        {
+            Gizmos.DrawLine(transform.position, movementPath[0].transform.position);
+        }
+
+        for (int i = 1; i < movementPath.Count; i++)
+        {
+            Gizmos.DrawLine(movementPath[i - 1].transform.position, movementPath[i].transform.position);
+        }
+    }
+
+    private void Start()
+    {
+        Vector3 startingPos = transform.position;
+
+        Sequence moveSequence = DOTween.Sequence();
+        Sequence rotateSequence = DOTween.Sequence();
+
+        for (int i = 0; i < movementPath.Count; i++)
+        {
+            float randNumb = Random.Range(4, 8);
+            moveSequence.Append(transform.DOMove(movementPath[i].transform.position, randNumb));
+            
+            Vector3 tempCurrent, tempNext;
+            if (i == 0)
+            {
+                tempCurrent = startingPos;
+                tempNext = movementPath[i].transform.position;
+            }
+            else
+            {
+                tempCurrent = movementPath[i - 1].transform.position;
+                tempNext = movementPath[i].transform.position;
+            }
+            tempCurrent.y = 0;
+            tempNext.y = 0;
+            Vector3 finalDir = tempNext - tempCurrent;
+
+            rotateSequence.Append(transform.DORotateQuaternion(Quaternion.LookRotation(finalDir, Vector3.up), randNumb));
+        }
+
+        if (movementPath.Count > 0)
+        {
+            float randNumb = Random.Range(4, 8);
+            moveSequence.Append(transform.DOMove(startingPos, randNumb));
+
+            Vector3 tempCurrent = movementPath[movementPath.Count - 1].transform.position, tempNext = startingPos;
+            tempCurrent.y = 0;
+            tempNext.y = 0;
+            Vector3 finalDir = tempNext - tempCurrent;
+
+            rotateSequence.Append(transform.DORotateQuaternion(Quaternion.LookRotation(finalDir, Vector3.up), randNumb));
+        }
+
+        moveSequence.SetLoops(-1);
+        moveSequence.Play();
+
+        rotateSequence.SetLoops(-1);
+        rotateSequence.Play();
+    }
+}
